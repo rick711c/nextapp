49435fb1e8821f2b7129fd29602c43e0578b48fa
saikat <rick711c@gmail.com>
2023-07-08 16:15:29 +0530

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Login from "./login/page"
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     hello world
      <Dashboard/>
      <Login/>
     </main>
